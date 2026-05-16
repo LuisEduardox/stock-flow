feed59cbb32b6d5890eae11ce6a74ecffe6183c2
Luis Eduardo <eduardogomessoares97@gmail.com>
2026-05-16 18:41:47 -0300

    feat: adicionar componentes navbar, aside, model de product, estilos tailwind e atualizar estrutura do app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Aside } from './components/aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Aside],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('stock-flow');
